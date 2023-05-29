202d8bd444d0c87028cbd4b7e9201e05373401e2
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-05-29 20:21:45 +0200

    added frontend forms LOGGIN & SIGNUP

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,112 @@
+"use client";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
+import React, { useCallback, useState } from "react";
+import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+type Variant = "LOGIN" | "SIGNUP";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("SIGNUP");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "SIGNUP") {
+      console.log(data);
+    }
+    if (variant === "LOGIN") {
+      console.log(data);
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lgsm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "SIGNUP" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            type="password"
+            label="Password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Se connecter" : "S'inscrire"}
+            </Button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Ou continuer avec
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+            <div>
+                {variant === "LOGIN" ? "Vous n'avez pas de compte?" : "Vous avez déjà un compte?"}
+            </div>
+            <div onClick={toggleVariant} className="underline cursor-pointer">
+                {variant === "LOGIN" ? "S'inscrire" : "Se connecter"}
+            </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
